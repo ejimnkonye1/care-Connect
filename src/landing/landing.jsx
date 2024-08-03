a3fb7e759c2c61e4a81a698d2076bc63434ae7d7
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-08-03 23:14:35 +0100

    new

--- src/landing/landing.jsx
+++ src/landing/landing.jsx
@@ -34,11 +34,11 @@ export const LandingPage = () => {
         <div className='container-fluid'>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="#">CareConnect</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-                <div className="collapse navbar-collapse d flex justify-content-end" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse d flex justify-content-end" id="navbarNavDropdown">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item ">
                             <a className="nav-link" href="#">Home</a>
