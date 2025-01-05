aa41b1f7d4a238c988da402c0ce602a2d43625e4
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-01-05 20:50:49 +0100

    rewrite admin login ui

--- src/Login/admin.jsx
+++ src/Login/admin.jsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
+import { auth, firestore  } from "../firebase"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {  signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc,  } from 'firebase/firestore';
 import logo from '../assets/image.png'
-export const AdminLoginForm = ({setIsAdminLogin, isAdminLogin,  setIsStaffLogin}) => {
+export const AdminLoginForm = ({setIsAdminLogin, isAdminLogin,  setIsStaffLogin,btnloading, setbtnloading}) => {
     const handleParentLogin = () => {
       setIsAdminLogin(!isAdminLogin);
     };
@@ -10,6 +15,53 @@ export const AdminLoginForm = ({setIsAdminLogin, isAdminLogin,  setIsStaffLogin}
       setIsStaffLogin(true)
 
     }
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+  const navigate = useNavigate()
+  const [passwordVisible, setPasswordVisible] = useState(false);
+    const handleLogin = async (e) => {
+      e.preventDefault();
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  
+        const user = userCredential.user
+  
+        const admindoc = await getDoc(doc(firestore, 'Admin', user.uid))
+        // console.log("User logged in: ", userCredential.user);
+        
+  
+        if (admindoc.exists()) {
+          setbtnloading(true)
+          setTimeout(() => {
+            setbtnloading(false)
+            navigate('/admin')
+          }, 9000);
+        
+        } else{
+    // User is not an admin, display an error message
+    setError("You are not authorized to access this dashboard.");
+    setTimeout(() => {
+      setError(false);
+    }, 5000);
+        }
+     
+      } catch (error) {
+        setbtnloading(true)
+        setTimeout(() => {
+          setbtnloading(false)
+          setError("Invalid email or password.")
+          setTimeout(() => {
+            setError(false)
+          }, 5000)
+        }, 2000)
+        console.error("Error logging in: ", error)
+      
+      }
+    };
+    const togglePasswordVisibility = () => {
+      setPasswordVisible(!passwordVisible);
+    };
     return(
       <div className="grid md:grid-cols-2 items-center gap-8">
       <div className="max-md:order-1 lg:min-w-[450px] hidden lg:block md:block ">
@@ -22,7 +74,7 @@ export const AdminLoginForm = ({setIsAdminLogin, isAdminLogin,  setIsStaffLogin}
       </div>
     
     
-      <form className="md:max-w-md w-full mx-auto">
+      <form className="md:max-w-md w-full mx-auto" onSubmit={handleLogin}>
              
        
         <div className="">
@@ -44,49 +96,32 @@ export const AdminLoginForm = ({setIsAdminLogin, isAdminLogin,  setIsStaffLogin}
               Staff Login
             </button>
           </div>
+          {error && (
+          <div className="bg-yellow-300 text-center p-2 rounded mb-4">
+            <span className="text-danger">{error}</span>
+          </div>
+        )}
         <div>
-          <div className="relative flex items-center">
+        <div className="relative flex items-center">
             <input
               name="email"
               type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
               className="w-full text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none"
               placeholder="Enter email"
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#bbb"
-              stroke="#bbb"
-              className="w-[18px] h-[18px] absolute right-2"
-              viewBox="0 0 682.667 682.667"
-            >
-              <defs>
-                <clipPath id="a" clipPathUnits="userSpaceOnUse">
-                  <path d="M0 512h512V0H0Z" data-original="#000000"></path>
-                </clipPath>
-              </defs>
-              <g clipPath="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
-                <path
-                  fill="none"
-                  strokeMiterlimit="10"
-                  strokeWidth="40"
-                  d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
-                  data-original="#000000"
-                ></path>
-                <path
-                  d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
-                  data-original="#000000"
-                ></path>
-              </g>
-            </svg>
           </div>
-        </div>
-    
-        <div className="mt-8">
-          <div className="relative flex items-center">
+      </div>
+  
+      <div className="mt-8">
+        <div className="relative flex items-center">
             <input
               name="password"
-              type="password"
+              type={passwordVisible ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               required
               className="w-full text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none"
               placeholder="Enter password"
@@ -97,11 +132,15 @@ export const AdminLoginForm = ({setIsAdminLogin, isAdminLogin,  setIsStaffLogin}
               stroke="#bbb"
               className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
               viewBox="0 0 128 128"
+              onClick={togglePasswordVisibility}
             >
-              <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+              <path
+                d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                data-original="#000000"
+              ></path>
             </svg>
           </div>
-        </div>
+      </div>
     
         <div className="flex flex-wrap items-center justify-between gap-4 mt-6">
           <div className="flex items-center">
@@ -116,24 +155,47 @@ export const AdminLoginForm = ({setIsAdminLogin, isAdminLogin,  setIsStaffLogin}
             </label>
           </div>
           <div>
-            <a href="javascript:void(0);" className="text-blue-600 font-semibold text-sm hover:underline">
+            <a href="reset" className="text-blue-600 font-semibold text-sm hover:underline">
               Forgot Password?
             </a>
           </div>
         </div>
     
         <div className="mt-12">
-          <button
-            type="button"
+        <button
+            type="submit"
             className="w-full shadow-xl py-2.5 px-5 text-sm font-semibold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
           >
-            Sign in
+            {btnloading ? (
+              <svg
+                className="animate-spin h-5 w-5 text-white mx-auto"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                role="status"
+                aria-hidden="true"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v2a6 6 0 100 12v2a8 8 0 01-8-8z"
+                ></path>
+              </svg>
+            ) : (
+              "Sign in"
+            )}
           </button>
-    
      
     
           <p className="text-gray-800 text-sm text-center mt-6">
-            Don't have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a>
+            Don't have an account <a href="/signup" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a>
           </p>
         </div>
       </form>
